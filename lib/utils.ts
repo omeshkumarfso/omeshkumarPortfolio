9c1a83140c0ec3655b8f120074b562d87ecee958
omeshkumar <omeshkumarfso@gmail.com>
2025-08-07 11:54:24 +0530

    hero section done

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const totalExperinceCalculate = (type: 'full' | 'short') => {
+  const experiences = [
+    { start: new Date('2023-11-01'), end: new Date() },
+    { start: new Date('2022-08-01'), end: new Date('2023-05-31') },
+    { start: new Date('2021-07-01'), end: new Date('2022-08-15') },
+    { start: new Date('2020-04-01'), end: new Date('2021-03-31') }
+  ];
+
+  const totalMonths = experiences.reduce((acc, { start, end }) => {
+    const months = (end.getFullYear() - start.getFullYear()) * 12 +
+    (end.getMonth() - start.getMonth());
+    return acc + months;
+  }, 0);
+
+  if (type === 'full') {
+    const years = Math.floor(totalMonths / 12);
+    const months = totalMonths % 12;
+    return `${years} ${years === 1 ? 'year' : 'years'}${months > 0 ? ` and ${months} ${months === 1 ? 'month' : 'months'}` : ''}`;
+  } else {
+    return `${(totalMonths / 12).toFixed(1)} years`;
+  }
+}
